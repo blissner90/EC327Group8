c27df59fd6a45f09695a6eea2d26e166c7e9b44c
blissner90 <blissner90@gmail.com>
2021-04-11 14:19:43 -0400

    Added to tasks and week header files

--- tasks.h
+++ tasks.h
@@ -1,6 +1,18 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
 class Task{
     
     public:
+        string taskName;
+        time_t currentTime;
+        time_t deadlineTime;
+        time_t remainingTime;
+        int expectedLength;
+        int pointsAwarded;
+        bool complete = false;
 
     private:
 
