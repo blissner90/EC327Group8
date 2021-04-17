7168533be7e486bd2e461a2d3b47d462f2e97860
jugarte1 <82297221+jugarte1@users.noreply.github.com>
2021-04-17 18:47:59 -0400

    Update tasks.h
    "tiffany and juan updating the user inputted time value for time deadline"

--- tasks.h
+++ tasks.h
@@ -7,6 +7,50 @@ using namespace std;
 // test
 // function to convert user inputted time to time_t structure
 
+// all inputs will be strings     scin == strings
+
+// testing getting time value from user
+time_t user_totime(string date, string time){
+    char* year, *month, *day, *hour, *min;
+    const char * dt = date.c_str();
+    const char * tm = time.c_str();
+    //cout<<dt<<endl;
+    char * temp1 = strdup(dt);
+    char * temp2 = strdup(tm);
+    //cout<<temp<<endl;
+
+    char * date1 = strtok_r(temp1, " ", &temp1);
+    year = strtok_r(date1, "-", &date1);
+    month = strtok_r(date1, "-", &date1);
+    day = strtok_r(date1, "-", &date1);
+
+    hour = strtok_r(temp2, ":", &temp2);
+    min = strtok_r(temp2, ":", &temp2);
+
+    time_t rawtime;
+    struct tm * timeinfo;
+    /* get current timeinfo and modify it to the user's choice */
+    time(&rawtime);
+    timeinfo = localtime ( &rawtime );
+    timeinfo->tm_year = atoi(year -1900);
+    timeinfo->tm_mon = atoi(month - 1);
+    timeinfo->tm_mday = atoi(day);
+    timeinfo->tm_hour = atoi(hour);
+    timeinfo->tm_min = atoi(min);
+
+  /* call mktime: timeinfo->tm_wday will be set */
+
+    time_t converted;
+    converted = mktime (timeinfo);
+    return converted;
+}
+
+//
+
+
+
+
+
 time_t user_totime(string givendeadline){
   int yyyy, month, day, hh, mm, ss;
 
@@ -46,11 +90,12 @@ class Task{
 };
 
 Task::Task(){
-  cin >> taskName; // give the name of the task
+  scin >> taskName; // give the name of the task
 
-  cin >> expectedLength  // how long you expect it to take in minutes
-
-  cin >> userdeadline;  // user inputs a string in a certain format; ex: 12:45:07
+  scin >> expectedLength;  // how long you expect it to take in minutes
+  expectedLength = stoi(expectedLength); // turn string into integer
+      
+  scin >> userdeadline;  // user inputs a string in a certain format; ex: 12:45:07
 
   deadlineTime = user_totime(userdeadline);  //string to time_t variable
 
